87076e474181b0c285414e15165d3ff34d26cfed
arielcou <arielcou@gmail.com>
2022-09-29 18:23:44 -0300

    first commit

---
+++ controller/cartManager.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+const pathToFile = "./src/data/products.json";
+
+class ProductsManager {
+
+  validacion(product) {
+    if (
+      !product.timestamp ||
+      !product.nombre ||
+      !product.descripcion ||
+      !product.codigo ||
+      !product.foto ||
+      !product.precio ||
+      !product.stock
+    )
+      return { status: "success", message: "missing fields" };
+  }
+  validacion(cart) {
+    if (!product.timestamp)
+      return { status: "success", message: "missing fields" };
+  }
+}
+
+module.exports = cartManager;
---
+++ router/productsRouter.js
@@ -0,0 +1,41 @@
+
+const express = require('express')
+const router = express.Router()
+
+const Manager = require('../controller/ProductsManager')
+const manager = new Manager()
+
+router.get("/", (req, res) => {
+    let productsAll = manager.findAllProducts()
+    res.send(productsAll);
+});
+  
+
+router.get("/:id", (req, res) => {
+    let productosId = manager.findProductById(req.params.id);
+    if (!productosId) return res.send({ error: "product was not found" });
+    res.send(productosId);
+});
+
+
+router.post('/', (req, res) => {
+    let result = manager.saveProduct(req.body)
+    res.send(result)
+});
+
+
+router.put('/:id', (req, res) => {
+    //if (!req.body.title || !req.body.price || !req.body.thumbnail) return res.send({error: 'data is required'})
+    let result = manager.updateProduct(req.params.id, req.body)
+    if (!result) return res.send({error: 'product was not found'})
+    res.send(result)
+})
+
+
+router.delete('/:id', (req, res) => {
+    let result = manager.deleteProducto(req.params.id)
+    res.send(result)
+})
+
+
+module.exports = router
